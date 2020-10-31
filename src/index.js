e30fa4263e10fec3b32a50e03a67fbc9ab39ad66
Matheus Mello <matheusmello_22@hotmail.com>
2020-10-31 16:40:51 -0300

    Add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/main.css";
-
-import App from "./App";
+import Routes from "./routes";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import "./assets/main.css";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
